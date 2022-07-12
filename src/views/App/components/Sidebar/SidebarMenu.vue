7b91dcb89aed6fa2d1a1be85ff8586eaabbb68b8
Alexey Kovalev <to.kovalev@gmail.com>
2022-07-12 22:19:49 +0300

    Рефакторинг компонентов sidebar'a

--- src/views/App/components/Sidebar/SidebarMenu.vue
+++ src/views/App/components/Sidebar/SidebarMenu.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue'
+import SidebarMenuItem from './SidebarMenuItem.vue'
 
 const props = defineProps({
   menu: {
@@ -13,20 +14,12 @@ const filteredMenu = computed(() => props.menu.filter((item) => item.show))
 
 <template>
   <div class="sidebar-menu">
-    <router-link
+    <SidebarMenuItem
       v-for="(item, idx) in filteredMenu"
       :key="idx"
-      :to="item.url"
+      :item="item"
       class="sidebar-menu__item"
-    >
-      <div class="sidebar-menu__left">
-        <img v-if="item.icon" class="sidebar-menu__icon" :src="item.icon" />
-        <div class="sidebar-menu__title">{{ item.title }}</div>
-      </div>
-      <div v-if="item.count !== 0" class="sidebar-menu__count">
-        {{ item.count }}
-      </div>
-    </router-link>
+    />
   </div>
 </template>
 
@@ -35,41 +28,7 @@ const filteredMenu = computed(() => props.menu.filter((item) => item.show))
   width: 100%;
 
   &__item {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    padding: 13px 24px;
     margin-top: 12px;
-    border-radius: 15px;
-    text-decoration: none;
-  }
-
-  &__item.router-link-active {
-    background: $menu-active-item;
-  }
-
-  &__left {
-    display: flex;
-    align-items: center;
-  }
-
-  &__icon {
-    width: 26px;
-    height: 26px;
-    margin-right: 14px;
-  }
-
-  &__title,
-  &__count {
-    color: $white;
-    font-size: 1.8rem;
-    line-height: 2.763rem;
-    font-weight: 600;
-  }
-
-  &__count {
-    color: $accent-2;
   }
 }
 </style>
---
+++ src/views/App/components/Sidebar/SidebarMenuItem.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <router-link :to="item.url" class="sidebar-menu-item">
+    <div class="sidebar-menu-item__left">
+      <img v-if="item.icon" class="sidebar-menu-item__icon" :src="item.icon" />
+      <div class="sidebar-menu-item__title">{{ item.title }}</div>
+    </div>
+    <div v-if="item.count !== 0" class="sidebar-menu-item__count">
+      {{ item.count }}
+    </div>
+  </router-link>
+</template>
+
+<style lang="scss">
+.sidebar-menu-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 13px 24px;
+  margin-top: 12px;
+  border-radius: 15px;
+  text-decoration: none;
+
+  &.router-link-active {
+    background: $menu-active-item;
+  }
+
+  &__left {
+    display: flex;
+    align-items: center;
+  }
+
+  &__icon {
+    width: 26px;
+    height: 26px;
+    margin-right: 14px;
+  }
+
+  &__title,
+  &__count {
+    color: $white;
+    font-size: 1.8rem;
+    line-height: 2.763rem;
+    font-weight: 600;
+  }
+
+  &__count {
+    color: $accent-2;
+  }
+}
+</style>
