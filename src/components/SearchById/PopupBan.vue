c4ce07aabdcba5680af5bebc8537d2dc64814eda
Mac <mac@Mac-mini.local>
2022-07-13 23:44:35 +0300

    searchById

---
+++ src/components/SearchById/PopupBan.vue
@@ -0,0 +1,92 @@
+<script setup>
+const emit = defineEmits(['closeBanModal'])
+</script>
+
+<template>
+  <div class="background__ban"
+  @click.self="emit('closeBanModal')">
+    <div class="container__ban">
+      <div class="content__ban">
+        <img  src="@/assets/search__logos/ban__logo__popup.svg" alt="" />
+        <p>Блокировка пользотваеля ID555222</p>
+        <textarea
+          class="desc__ban"
+          placeholder="Опишите причину блокирови...
+"
+        ></textarea>
+        <div class="btn">Заблокировать</div>
+      </div>
+      <div class="logo__close">
+        <img @click="emit('closeBanModal')" src="@/assets/search__logos/close.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.background__ban {
+  z-index: 100;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  width: 1680px;
+  background: rgba(11, 12, 18, 0.9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+        .logo__close {
+          cursor: pointer;
+      }
+  .container__ban {
+    display: flex;
+    .content__ban {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 24px;
+      background: #242529;
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      box-shadow: 0px 12px 40px rgba(0, 0, 0, 0.17);
+      border-radius: 24px;
+      margin-right: 16px;
+
+      p {
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 153.5%;
+        margin: 16px 0 0 0;
+      }
+      .desc__ban {
+        padding: 16px;
+        margin: 16px 0 16px 0;
+        width: 328px;
+        height: 215px;
+        outline: none;
+        resize: none;
+        border: 1px solid #ffffff;
+        border-radius: 11px;
+        background: none;
+        color: #ffffff;
+        &::placeholder {
+          font-weight: 600;
+          font-size: 15px;
+          line-height: 153.5%;
+          color: rgba(255, 255, 255, 0.33);
+        }
+      }
+      .btn {
+        width: 328px;
+        height: 54px;
+        background: linear-gradient(137.15deg, #2965ff 0%, #2e66f5 99.89%);
+        border-radius: 11px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/SearchById/PopupDiamond.vue
@@ -0,0 +1,129 @@
+<script setup>
+const emit = defineEmits(['closeDiamondModal'])
+</script>
+
+<template>
+  <div class="background__diamond" @click.self="emit('closeDiamondModal')">
+    <div class="container__diamond">
+      <div class="content__diamond">
+        <img src="@/assets/search__logos/diamond.svg" alt="" class="logo__diamond"/>
+        <p>Присвоить пользователю ID454543 статус Diamond?</p>
+        <div class="btn__block">
+          <div class="btn" @click="emit('closeDiamondModal')">Отмена</div>
+          <div class="btn gold">Присвоить</div>
+        </div>
+      </div>
+      <div class="close__logo">
+        <img
+          src="@/assets/search__logos/close.svg"
+          alt=""
+          @click="emit('closeDiamondModal')"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.background__diamond {
+  z-index: 100;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  width: 1680px;
+  background: rgba(11, 12, 18, 0.9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .container__diamond {
+    .close__logo {
+      margin-left: 16px;
+      cursor: pointer;
+    }
+    display: flex;
+    .content__diamond {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      background: rgba(36, 37, 41, 0.6);
+      border-radius: 24px;
+      width: 376px;
+      height: 234px;
+      .logo__diamond {
+          margin-top: 24px;
+      }
+      p {
+        margin: 10px 0 16px 0;
+        width: 308px;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 153.5%;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        background: linear-gradient(
+          132.27deg,
+          #f9ae3f -0.77%,
+          #ffdba5 36.67%,
+          #ad6902 96.37%
+        );
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        text-fill-color: transparent;
+      }
+      .btn__block {
+        display: flex;
+        width: 328px;
+        justify-content: space-between;
+        .btn {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 156px;
+          height: 54px;
+          border: 1px solid rgba(255, 255, 255, 0.33);
+          border-radius: 11px;
+          cursor: pointer;
+          position: relative;
+          z-index: 3;
+          &.gold {
+            border: none;
+            background: linear-gradient(
+              132.27deg,
+              #f9ae3f -0.77%,
+              #ffdba5 36.67%,
+              #ad6902 96.37%
+            );
+            color: #000000;
+          }
+        }
+      }
+    }
+    .content__diamond:before {
+      background: linear-gradient(
+        103.31deg,
+        #f9ae3f 100%,
+        #ffdba5 100%,
+        #ad6902 100%
+      );
+    }
+    .content__diamond:before {
+      content: '';
+      width: 376px;
+      height: 234px;
+      position: absolute;
+      border-radius: 24px;
+      padding: 1px;
+      -webkit-mask: linear-gradient(#fff, #fff 0) content-box,
+        linear-gradient(#fff, #fff 0);
+      -webkit-mask: linear-gradient(#fff 0 0) content-box,
+        linear-gradient(#fff 0 0);
+      -webkit-mask-composite: xor;
+      mask-composite: exclude;
+    }
+  }
+}
+</style>
