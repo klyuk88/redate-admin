26b3c14c6b447a5c1ab2fdaddb254f34dcbd8d32
Alexey Kovalev <to.kovalev@gmail.com>
2022-07-16 02:22:55 +0300

    Добавлена верстка таба Отклоненные

---
+++ src/views/Mailings/components/MailingsRejectedList.vue
@@ -0,0 +1,36 @@
+<script setup>
+import MailingsRejectedListItem from './MailingsRejectedListItem.vue'
+
+defineProps({
+  mailings: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="mailings-rejected-list">
+    <MailingsRejectedListItem
+      v-for="(mailing, idx) in mailings"
+      :key="idx"
+      class="mailings-rejected-list__item"
+      :mailing="mailing"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+.mailings-rejected-list {
+  height: calc(100vh - 135px);
+  padding-right: 24px;
+
+  &__item {
+    margin-top: 32px;
+
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+}
+</style>
--- src/views/Mailings/views/MailingsRejected.vue
+++ src/views/Mailings/views/MailingsRejected.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { computed } from 'vue'
+import { useMailingsStore } from '../store/Mailings'
+import Scrollbar from '@/components/Scrollbar'
+import MailingsRejectedList from '../components/MailingsRejectedList.vue'
+
+const mailingsStore = useMailingsStore()
+
+const mailings = computed(() => mailingsStore.rejectedMailings)
+</script>
+
 <template>
-  <div>Отклоненные</div>
+  <div class="mailings-rejected">
+    <div class="mailings-rejected__content">
+      <Scrollbar>
+        <MailingsRejectedList
+          class="mailings-rejected__list"
+          :mailings="mailings"
+        />
+      </Scrollbar>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.mailings-rejected {
+  height: 100%;
+  border-left: 1px solid rgba($white, 0.066);
+  border-right: 1px solid rgba($white, 0.066);
+  padding: 36px 48px 0 48px;
+}
+</style>
